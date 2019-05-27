02f5ce4f4a0a8df6d1be331acf485a45052c8ced
Winston Chen <weijiechen1994@gmail.com>
2019-05-27 17:34:18 -0500

    Create setup.py

---
+++ recipes/deeprad_basic/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='DeepRad_basic',
+    version='1.0.0',
+    packages=['dr_basic',],
+    license='DeepRad license',
+    long_description=open('README.txt').read(),
+)
